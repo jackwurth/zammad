0ab7a8f3a8f7e3b13ee8f505589335f11181eabe
Tobias Schäfer <github@blackox.org>
2023-08-04 16:49:16 +0200

    Rename files.

--- contrib/packager.io/postinstall.sh
+++ contrib/packager.io/postinstall.sh
@@ -3,4 +3,46 @@
 # packager.io postinstall script
 #
 
+# base dir
+ZAMMAD_DIR=${ZAMMAD_DIR:="/opt/zammad"}
 
+# import config
+source ${ZAMMAD_DIR}/contrib/packager.io/config
+
+PATH="${ZAMMAD_DIR}/bin:/opt/zammad/vendor/bundle/bin:/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin"
+
+# import functions
+source ${ZAMMAD_DIR}/contrib/packager.io/lib/misc.sh
+source ${ZAMMAD_DIR}/contrib/packager.io/lib/ui.sh
+source ${ZAMMAD_DIR}/contrib/packager.io/lib/service/database/batch.sh
+source ${ZAMMAD_DIR}/contrib/packager.io/lib/service/redis/batch.sh
+source ${ZAMMAD_DIR}/contrib/packager.io/lib/service/elasticsearch/batch.sh
+source ${ZAMMAD_DIR}/contrib/packager.io/lib/service/proxy/batch.sh
+
+function detect_update() {
+  DB_UPDATE="no"
+  REDIS_UPDATE="no"
+  PROXY_UPDATE="no"
+  ZAMMAD_UPDATE="no"
+
+  export DB_UPDATE REDIS_UPDATE PROXY_UPDATE ZAMMAD_UPDATE
+}
+
+# exec postinstall
+detect_os
+
+detect_initcmd
+
+detect_update
+
+ui_welcome
+
+database_run
+
+redis_run
+
+elasticsearch_run
+
+proxy_run
+
+update_or_install
