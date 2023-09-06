5a35d95f6cc1c43ca043f2634a1088a4b461cf0f
Tobias Schäfer <github@blackox.org>
2023-09-06 14:33:15 +0200

    Improve skip es repo setup.

--- contrib/packager.io/lib/service/elasticsearch/script.sh
+++ contrib/packager.io/lib/service/elasticsearch/script.sh
@@ -1,13 +1,13 @@
 function elasticsearch_server_install() {
   case ${OS} in
     DEBIAN)
+      apt-get install apt-transport-https
       if [ ! -f /etc/apt/sources.list.d/elastic-8.x.list ]; then
         curl --silent --location https://artifacts.elastic.co/GPG-KEY-elasticsearch | \
           gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
+        echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | \
+          tee /etc/apt/sources.list.d/elastic-8.x.list
       fi
-      apt-get install apt-transport-https
-      echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | \
-        tee /etc/apt/sources.list.d/elastic-8.x.list
       apt-get update
       apt-get install elasticsearch
       ;;
