9953db7bce15a306c4600d443d0971a6529db868
Tobias Schäfer <github@blackox.org>
2023-09-18 10:52:00 +0200

    Fix Ruby call.

--- contrib/packager.io/lib/service/database/script.sh
+++ contrib/packager.io/lib/service/database/script.sh
@@ -59,5 +59,5 @@ EOF
 }
 
 function database_server_verify_connection() {
-    ruby -e "$(database_server_verify_connection_script)"
+    zammad run ruby -e "$(database_server_verify_connection_script)"
 }
